7ad2c2aaff2ac3deb9f6a3704a9b7cbf7ebc150b
André Gross <andgross@gmail.com>
2022-09-21 18:30:32 -0300

    css finalizado, iniciando as funções

--- src/components/Actions.jsx
+++ src/components/Actions.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
 
 function Actions() {
   return (
-    <div className='ac-container'>Actions</div>
-  )
+    <div className="ac-container">
+      <div className="btn-container">
+        <button>Inciar</button>
+        <button>Tempo</button>
+      </div>
+      <form>
+        <input type="text" placeholder="Quanto tempo temos?" />
+      </form>
+      <div className="used-container">
+        <div className="used-timers">
+          <h2>Usados recentemente</h2>
+          <ul>
+            <li>
+              <button>1 min</button>
+            </li>
+            <li>
+              <button>5 min</button>
+            </li>
+            <li>
+              <button>10 min</button>
+            </li>
+            <li>
+              <button>15 min</button>
+            </li>
+          </ul>
+        </div>
+        <div className="reset">
+          <button>Limpar</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Actions
+export default Actions;
